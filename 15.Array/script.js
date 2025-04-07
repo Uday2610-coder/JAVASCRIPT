cb827c23690f66f4129dc5d04683b2a8410cf452
Uday2610-coder <mistryuday2610@gmail.com>
2025-04-07 11:41:33 +0530

    Upload file

---
+++ 15.Array/script.js
@@ -0,0 +1,23 @@
+const frien1 = 'Michael';
+const frien2 = 'Steven';
+const frien3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const years = new Array[1991, 1984, 2008, 2020];
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log*(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtman', 2037 - 1991, 'teacher', friends];
+
+console.log(jonas);
+console.log(jonas.length);
